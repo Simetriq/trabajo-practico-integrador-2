b35b0be727ecd7b72781b2651d51203b353698be
NatZigaran. <natanaelzigaran@gmail.com>
2025-09-23 15:28:03 -0300

    se creo el controlador para crear un comentario

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,7 +8,13 @@ export const createComment = async (req, res) => {
       author,
       article,
     });
+    return res.status(200).json({
+      data: newComment,
+      ok: true,
+      msg: "Se creo un comentario exitosamente",
+    });
   } catch (error) {
+    console.log(error);
     return res.status(400).json({
       ok: false,
       msg: "Error interno del servidor",
